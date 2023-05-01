977fde38e4fbaecb0b03f33c05ba9f77317d9d7c
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-01 19:29:09 -0400

    Implemented genre schema.

--- models/genre.js
+++ models/genre.js
@@ -6,3 +6,26 @@
 /*-----------------------------------------------------------------------------
   IMPORTS
 -----------------------------------------------------------------------------*/
+const mongoose = require("mongoose");
+
+const Schema = new mongoose.Schema;
+
+/**
+ * Schema for a particular genre
+ * @constructor Genre
+ */
+const GenreSchema = new Schema({
+    name: { type: String, required: true, minLength: 3, maxLength: 50 },
+});
+
+
+/**
+ * Virtural for genre's URL.
+ * @virtual url
+ */
+GenreSchema.virtual("url").get(function() {
+    return `/inventory/genre/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Genre", GenreSchema);
