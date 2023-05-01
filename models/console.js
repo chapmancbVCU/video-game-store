d1a4c684f37203e083eb85111abb0f6913a5521d
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-01 19:43:46 -0400

    Implemented schema for console.

--- models/console.js
+++ models/console.js
@@ -6,3 +6,38 @@
 /*-----------------------------------------------------------------------------
   IMPORTS
 -----------------------------------------------------------------------------*/
+const mongoose = require("mongoose");
+
+
+/**
+ * Schema constant.
+ * @type Schema
+ */
+const Schema = mongoose.Schema;
+
+/**
+ * Schema for a concole.
+ * @constructor Console
+ */
+const ConsoleSchema = new Schema({
+    name: { type: String, required: true },
+    manufacturer: {
+        type: String,
+        required: true,
+        enum: ["PC", "Sony", "Microsoft", "Nintendo"],
+        default: "PC",
+    },
+    description: { type: String, required: true},
+    imageFile: { type: String, required: true },
+});
+
+/**
+ * Virtual for concole's URL.
+ * @virtual url
+ */
+ConsoleSchema.virtual("url").get(function() {
+    return `/inventory/console/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Console", ConsoleSchema);
