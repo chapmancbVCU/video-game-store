3afbf8a47a6b821b12e90fd6d5040a0c54a2a094
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-03 19:27:00 -0400

    Implemented console instance schema.

--- models/consoleinstance.js
+++ models/consoleinstance.js
@@ -6,3 +6,40 @@
 /*-----------------------------------------------------------------------------
   IMPORTS
 -----------------------------------------------------------------------------*/
+const mongoose = require("mongoose");
+
+
+/**
+ * Schema constant.
+ * @type Schema
+ */
+const Schema = mongoose.Schema;
+
+
+/**
+ * Schema for gaming console instance.
+ * @constructor Gaming Console Instance.
+ */
+const ConsoleInstanceSchema = new Schema({
+    storeNumber: { type: Schema.Types.ObjectID, ref: "StoreLocation" },
+    comments: { 
+        type: String, 
+        required: true, 
+        minLength: 3, 
+        maxLength: 255 
+    },
+    reserved: { type: Boolean },
+});
+
+
+/**
+ * Virtual for console instance URL.
+ * @virtual url
+ */
+ConsoleInstanceSchema.virtual("url").get(function() {
+    return `/inventory/consoleinstance/${this._id}`;
+});
+
+
+// Export model
+module.exports = mongoose.model("ConsoleInstance", ConsoleInstanceSchema);
