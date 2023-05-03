1cae0e77008f5ab65c681e3fc660d551621f82a0
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-03 19:29:31 -0400

    Implemented game instance schema.

--- models/gameinstance.js
+++ models/gameinstance.js
@@ -6,3 +6,40 @@
 /*-----------------------------------------------------------------------------
   IMPORTS
 -----------------------------------------------------------------------------*/
+const mongoose = require("mongoose");
+
+
+/**
+ * Schema constant.
+ * @type Schema
+ */
+const Schema = mongoose.Schema;
+
+
+/**
+ * Schema for game instance.
+ * @constructor Game Instance.
+ */
+const GameInstanceSchema = new Schema({
+    storeNumber: { type: Schema.Types.ObjectID, ref: "StoreLocation" },
+    comments: { 
+        type: String, 
+        required: true, 
+        minLength: 3, 
+        maxLength: 255 
+    },
+    reserved: { type: Boolean },
+});
+
+
+/**
+ * Virtual for game instance URL.
+ * @virtual url
+ */
+GameInstanceSchema.virtual("url").get(function() {
+    return `/inventory/gameinstance/${this._id}`;
+});
+
+
+// Export model
+module.exports = mongoose.model("GameInstance", GameInstanceSchema);
