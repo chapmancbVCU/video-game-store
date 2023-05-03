86c71dd1d275a48fbfde1629ef63b1b90972ddff
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-03 17:34:52 -0400

    Created schema for game.

--- models/game.js
+++ models/game.js
@@ -6,3 +6,58 @@
 /*-----------------------------------------------------------------------------
   IMPORTS
 -----------------------------------------------------------------------------*/
+const mongoose = require("mongoose");
+
+
+/**
+ * Schema constant.
+ * @type Schema
+ */
+const Schema = mongoose.Schema;
+
+
+/**
+ * Schema for game.
+ * @constructor Game
+ */
+const GameSchema = new Schema({
+    title: { type: String, required: true },
+    gamingConsole: [{ type: Schema.Types.ObjectID, ref: "GameConsole" }],
+    description: { type: String, required: true },
+    genre: [{ type: Schema.Types.ObjectID, ref: "Genre" }],
+    imageFile: { type: String, required: true },
+    rating: {
+        type: String,
+        required: true,
+        enum: ["E", "E10", "T", "M", "A", "RP", "RP17"],
+        default: "RP",
+    },
+    publisher: { type: String, required: true},
+    condition: {
+        type: String,
+        required: true,
+        enum: ["New", "Used"],
+        default: "New",
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["Available", "Sold Out", "Pre-Order"]
+    },
+    releaseDate: { type: Date },
+    price: { type: Double, required: true },
+    upc: { type: Number, required: true },
+});
+
+
+/**
+ * Virtual for game's URL.
+ * @virtual url
+ */
+GameSchema.virtual("url").get(function() {
+    return `/inventory/game/${this._id}`;
+});
+
+
+// Export model
+module.exports = mongoose.model("Game", GameSchema);
