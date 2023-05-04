ed53f085bec0d2e51e230a0256ddf40c0c3b2cac
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-04 19:22:54 -0400

    Created JSDOC comments.

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -1,46 +1,82 @@
+/**
+ * @file Controller file for game.
+ * @author Chad Chapman
+ */
+
+/*-----------------------------------------------------------------------------
+  IMPORTS
+-----------------------------------------------------------------------------*/
 const Game = require("../models/game");
 const asyncHandler = require('express-async-handler')
 
+
+/**
+ * @property Home page
+ */
 exports.index = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Site Home Page");
 });
 
-// Display list of all games.
+
+/**
+ * @property Display list of all games.
+ */
 exports.game_list = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: game list");
 });
 
-// Display detail page for a specific game.
+
+/**
+ * @property Display detail page for a specific game.
+ */
 exports.game_detail = asyncHandler(async (req, res, next) => {
   res.send(`NOT IMPLEMENTED: game detail: ${req.params.id}`);
 });
 
-// Display game create form on GET.
+
+/**
+ * @property Display game create form on GET.
+ */
 exports.game_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: game create GET");
 });
 
-// Handle game create on POST.
+
+/**
+ * @property Handle game create on POST.
+ */
 exports.game_create_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: game create POST");
 });
 
-// Display game delete form on GET.
+
+/**
+ * @property Display game delete form on GET.
+ */
 exports.game_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: game delete GET");
 });
 
-// Handle game delete on POST.
+
+/**
+ * @property Handle game delete on POST.
+ */
 exports.game_delete_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: game delete POST");
 });
 
-// Display game update form on GET.
+
+/**
+ * @property Display game update form on GET.
+ */
 exports.game_update_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: game update GET");
 });
 
-// Handle game update on POST.
+
+/**
+ * @property Handle game update on POST.
+ */
 exports.game_update_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: game update POST");
 });
