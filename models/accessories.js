f0929f197bd91abffa229dd32bc10ab0b658f536
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-05 18:19:43 -0400

    Created schema for accessories.

---
+++ models/accessories.js
@@ -0,0 +1,37 @@
+/**
+ * @file Schema for accessories.
+ * @author Chad Chapman
+ */
+
+/*-----------------------------------------------------------------------------
+  IMPORTS
+-----------------------------------------------------------------------------*/
+const mongoose = require("mongoose");
+
+
+/**
+ * Schema constant.
+ * @type Schema
+ */
+const Schema = mongoose.Schema;
+
+
+const AccessoriesSchema = new Schema({
+    name: { type: String, required: true },
+    upc: { type: Number, required: true },
+    platform: [{ type: Schema.Types.ObjectID, ref: "platform"} ],
+    description: { type: String, required: true },
+})
+
+
+/**
+ * Virtual for accessories's URL.
+ * @virtual url
+ */
+GameSchema.virtual("url").get(function() {
+    return `/inventory/accessories/${this._id}`;
+});
+
+
+// Export model
+module.exports = mongoose.model("Accessories", AccessoriesSchema);
