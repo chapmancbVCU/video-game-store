8d443886ddf9f847ed03e4002a032b80cf21962f
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-05 18:33:56 -0400

    Initial commit and imports.

---
+++ routes/inventory.js
@@ -0,0 +1,74 @@
+/**
+ * @file Inventory route module.
+ * @author Chad Chapman
+ */
+
+/*-----------------------------------------------------------------------------
+  IMPORTS
+-----------------------------------------------------------------------------*/
+const express = require("express");
+const router = express.Router();
+
+
+// Require controller modules.
+const accessories_controller = require("../controllers/accessoriescontroller");
+const consoleinstance_controller = require(
+    "../controllers/consoleinstancecontroller");
+const gameconsoleinstance_controller = require(
+    "../controllers/gameinstancecontroller");
+const game_controller = require("../controllers/gamecontroller");
+const gameinstance_controller = require(
+    "../controllers/gameinstancecontroller");
+const genre_controller = require("../controllers/genrecontroller");
+const platform_controller = require("../controllers/platformcontroller");
+const storelocation_controller = require(
+    "../controllers/storelocationcontroller");
+
+
+/*-----------------------------------------------------------------------------
+  ACCESSORIES ROUTES
+-----------------------------------------------------------------------------*/
+
+
+
+/*-----------------------------------------------------------------------------
+  CONSOLE INSTANCE ROUTES
+-----------------------------------------------------------------------------*/
+
+
+
+/*-----------------------------------------------------------------------------
+  GAME CONSOLE ROUTES
+-----------------------------------------------------------------------------*/
+
+
+
+/*-----------------------------------------------------------------------------
+  GAME ROUTES
+-----------------------------------------------------------------------------*/
+
+
+
+/*-----------------------------------------------------------------------------
+  GAME INSTANCE ROUTES
+-----------------------------------------------------------------------------*/
+
+
+
+/*-----------------------------------------------------------------------------
+  GENRE ROUTES
+-----------------------------------------------------------------------------*/
+
+
+
+/*-----------------------------------------------------------------------------
+  PLATFORM ROUTES
+-----------------------------------------------------------------------------*/
+
+
+
+/*-----------------------------------------------------------------------------
+  STORE LOCATION ROUTES
+-----------------------------------------------------------------------------*/
+
+
