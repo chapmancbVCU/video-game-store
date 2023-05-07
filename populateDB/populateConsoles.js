abcb3b94769f682458f857928c0f25df323a3c3f
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-07 11:51:33 -0400

    Updated script to reflect changes in schema.

--- populateDB/populateConsoles.js
+++ populateDB/populateConsoles.js
@@ -4,10 +4,10 @@ const userArgs = process.argv.slice(2);
  * database.
  * @author Chad Chapman
  */
-const Console = require("../models/consoleinstance");
+const Console = require("../models/gameconsole");
 const consoles = [];
-
 const mongoose = require("mongoose");
+const gameconsole = require("../models/gameconsole");
 mongoose.set("strictQuery", false); // Prepare for Mongoose 7
 
 const mongoDB = userArgs[0];
@@ -25,23 +25,31 @@ async function main() {
 
 async function consoleCreate(
     name, 
-    manufacturer, 
+    platform,
+    accessories,
+    gamesIncluded, 
     description, 
     imageFile,
     condition,
     status,
     price,
     upc) {
-    const consoleObj = new Console({ 
+    consoleObjDetail = { 
         name: name,
-        manufacturer: manufacturer,
+        //platform: platform,
+        //accessories: accessories,
+        //gamesIncluded: gamesIncluded,
         description: description,
         imageFile: imageFile,
         condition: condition,
         status: status,
         price: price,
         upc: upc,
-    });
+    };
+    if (accessories != false) consoleObjDetail.accessories = accessories;
+    if (gamesIncluded != false) consoleObjDetail.gamesIncluded = gamesIncluded;
+    if (platform != false) consoleObjDetail.platform = platform;
+    const consoleObj = new Console(consoleObjDetail);
     await consoleObj.save();
     consoles.push(consoleObj);
     console.log(`Added Console: ${name}`);
@@ -52,7 +60,9 @@ async function createConsoles() {
     await Promise.all([
         consoleCreate(
             "Xbox Series X", 
-            "Microsoft",
+            false,
+            false,
+            false,
             "Introducing Xbox Series X, the fastest, most powerful Xbox ever. Play thousands of titles from four generations of consoles—all games look and play best on Xbox Series X. At the heart of Series X is the Xbox Velocity Architecture, which pairs a custom SSD with integrated software for faster, streamlined gameplay with significantly reduced load times. Seamlessly move between multiple games in a flash with Quick Resume. Explore rich new worlds and enjoy the action like never before with the unmatched 12 teraflops of raw graphic processing power. Enjoy 4K gaming at up to 120 frames per second, advanced 3D spatial sound, and more.",
             "pc.png",
             "New",
