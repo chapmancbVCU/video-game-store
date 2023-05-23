00a17adb014d2c240f3b6348261c09bac493e88d
Chad Chapman <chad.chapman2010@gmail.com>
2023-05-22 21:20:47 -0400

    We can upload images now.

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -13,6 +13,8 @@ const Genre = require("../models/genre");
 const Platform = require("../models/platform");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require('express-async-handler')
+const multer = require('multer');
+const path = require('path');
 
 
 /**
@@ -103,6 +105,32 @@ exports.game_create_post = [
         next();
     },
 
+    /*(req, res, next) => {
+        
+        try {
+            let storage = multer.diskStorage({
+                destination: (req, file, cb) => {
+                    cb(null, 'public/images/uploads');
+                },
+                filename: (req, file, cb) => {
+                    fileName = Date.now() + file.originalname;
+                    cb(null, Date.now() + file.originalname)
+                }
+            })
+            let upload = multer({ storage: storage }).single('imageFile');
+            upload(req, res, function (err) {
+                if (err) {
+                    console.log(err);
+                    return res.end("Error uploading file.");
+                } else {
+                    res.end("File has been uploaded");
+                }
+            });
+        } catch (error) {
+
+        }
+        next();
+    },*/
     // Validate and sanitize fields.
     body("title", "Title must not be empty.")
         .trim()
@@ -118,7 +146,7 @@ exports.game_create_post = [
         .escape(),
     body("platform.*").escape(),
     body("genre.*").escape(),
-    //body("imageFile", "Must select an image").escape(),
+    body("imageFile", "Must select an image").escape(),
     body("rating").escape(),
     body("condition").escape(),
     body("status").escape(),
@@ -140,14 +168,38 @@ exports.game_create_post = [
     asyncHandler(async (req, res, next) => {
         // Extract the validation errors from a request.
         const errors = validationResult(req);
+        let fileName = 'foo'
+        try {
+            let storage = multer.diskStorage({
+                destination: (req, file, cb) => {
+                    cb(null, 'public/images/uploads');
+                },
+                filename: (req, file, cb) => {
+                    fileName = Date.now() + file.originalname;
+                    cb(null, Date.now() + file.originalname)
+                }
+            })
+            let upload = multer({ storage: storage }).single('imageFile');
+            upload(req, res, function (err) {
+                if (err) {
+                    console.log(err);
+                    return res.end("Error uploading file.");
+                } else {
+                    res.end("File has been uploaded");
+                }
+            });
+        } catch (error) {
 
+        }
+
+        console.log("filename: " + fileName)
         // Create a Game Object.
         const game = new Game({
             title: req.body.title,
             platform: req.body.platform,
             genre: req.body.genre,
             description: req.body.description,
-           // imageFile: req.body.imageFile,
+            imageFile: req.body.imageFile,
             rating: req.body.rating,
             publisher: req.body.publisher,
             condition: req.body.condition,
