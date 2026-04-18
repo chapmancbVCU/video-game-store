09a3641916f8701c258b50e451ec2691d5fa2f74
Chad Chapman <chad.chapman2010@gmail.com>
2026-04-18 17:59:23 -0400

    Initial commit

---
+++ database/migrations/MDT20260418215821CreateProductsTable.php
@@ -0,0 +1,41 @@
+<?php
+namespace Database\Migrations;
+use Core\Lib\Database\Schema;
+use Core\Lib\Database\Blueprint;
+use Core\Lib\Database\Migration;
+
+/**
+ * Migration class for the products table.
+ */
+class MDT20260418215821CreateProductsTable extends Migration {
+    /**
+     * Performs a migration for a new table.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 155);
+            $table->decimal('price', 10, 2);
+            $table->decimal('list', 10, 2);
+            $table->decimal('shipping', 10, 2);
+            $table->tinyInteger('featured', 1)->nullable();
+            $table->text('description');
+            $table->softDeletes();
+            $table->index('featured');
+            $table->integer('brand_id');
+            $table->index('brand_id');
+        });
+    }
+
+    /**
+     * Undo a migration task.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists('products');
+    }
+}
