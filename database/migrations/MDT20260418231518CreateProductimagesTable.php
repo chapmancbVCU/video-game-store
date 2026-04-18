10e9a4496fdd4568e63f647c022e7433de43fd16
Chad Chapman <chad.chapman2010@gmail.com>
2026-04-18 19:15:59 -0400

    Initial commit

---
+++ database/migrations/MDT20260418231518CreateProductimagesTable.php
@@ -0,0 +1,37 @@
+<?php
+namespace Database\Migrations;
+use Core\Lib\Database\Schema;
+use Core\Lib\Database\Blueprint;
+use Core\Lib\Database\Migration;
+
+/**
+ * Migration class for the productimages table.
+ */
+class MDT20260418231518CreateProductimagesTable extends Migration {
+    /**
+     * Performs a migration for a new table.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('url', 255);
+            $table->integer('sort')->nullable();
+            $table->integer('product_id');
+            $table->string('name', 255);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->index('product_id');
+        });
+    }
+
+    /**
+     * Undo a migration task.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists('productimages');
+    }
+}
